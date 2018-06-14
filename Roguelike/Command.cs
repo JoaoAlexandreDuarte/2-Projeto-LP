a99fb19eed62ac7b0c9860794f445494b0922c55
João Duarte <onlyneplayer@gmail.com>
2018-06-14 14:03:01 +0100

    Adicionar enum Command

---
+++ Roguelike/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Roguelike {
+    [Flags]
+    public enum Command {
+        None = 0,
+        Quit = 1,
+        MoveUp = 2,
+        MoveDown = 4,
+        MoveLeft = 8,
+        MoveRight = 16,
+        AttackNPC = 32,
+        PickUpItem = 64,
+        UseItem = 128,
+        DropItem = 256,
+        Information = 512
+    }
+}
