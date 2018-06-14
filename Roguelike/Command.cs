879463350e28d52f04b43cbadcefb966f4042d64
João Duarte <onlyneplayer@gmail.com>
2018-06-14 15:05:24 +0100

    Adicionar enum com os possíveis comandos a efetuar durante o jogo

--- Roguelike/Command.cs
+++ Roguelike/Command.cs
@@ -5,14 +5,14 @@ namespace Roguelike {
     public enum Command {
         None = 0,
         Quit = 1,
-        MoveUp = 2,
-        MoveDown = 4,
-        MoveLeft = 8,
-        MoveRight = 16,
+        MoveNorth = 2,
+        MoveSouth = 4,
+        MoveWest = 8,
+        MoveEast = 16,
         AttackNPC = 32,
         PickUpItem = 64,
         UseItem = 128,
         DropItem = 256,
-        Information = 512
+        Information = 512,
     }
 }
