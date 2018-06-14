35efc15d93bbba40fb0795f06d67d450cc0f7747
João Duarte <onlyneplayer@gmail.com>
2018-06-14 15:08:28 +0100

    Adicionar a limpeza do level quando passa ao próximo

--- Roguelike/LevelGenerator.cs
+++ Roguelike/LevelGenerator.cs
@@ -8,21 +8,36 @@ namespace Roguelike {
     public class LevelGenerator {
         Random rnd = new Random();
 
-        public Tuple<int, int, int, int> GenerateLevel(World world,
-            Player player, int level) {
+        public Tuple<int, int> GenerateLevel(World world, Player player,
+            int level) {
+
+            if (level > 1) {
+                CleanLevel(world);
+            }
+
             int rowPlayer, rowExit;
 
             rowPlayer = rnd.Next(world.X);
 
             world.WorldArray[rowPlayer, 0].AddTo(player);
             world.WorldArray[rowPlayer, 0].IsVisible = true;
+            player.X = rowPlayer;
+            player.Y = 0;
 
             rowExit = rnd.Next(world.X);
 
             world.WorldArray[rowExit, 7].AddTo(0);
             world.WorldArray[rowExit, 7].IsExit = true;
 
-            return new Tuple<int, int, int, int>(rowPlayer, 0, rowExit, 7);
+            return new Tuple<int, int>(rowExit, 7);
+        }
+
+        private void CleanLevel(World world) {
+            for (int row = 0; row < world.X; row++) {
+                for (int column = 0; column < world.Y; column++) {
+                    world.WorldArray[row, column] = new Tile(world.TileSize);
+                }
+            }
         }
     }
 }
