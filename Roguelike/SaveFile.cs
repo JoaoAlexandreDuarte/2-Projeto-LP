98bb3082a46932a4710f4fdbf8a79df8ce80e5b5
João Duarte <onlyneplayer@gmail.com>
2018-06-20 22:24:03 +0100

    Adicionar o savefile

---
+++ Roguelike/SaveFile.cs
@@ -0,0 +1,15 @@
+﻿namespace Roguelike {
+    public class SaveFile {
+        public Player Player { get; set; }
+        public World World { get; set; }
+        public int Level { get; set; }
+        public int[] ExitPos { get; set; }
+
+        public SaveFile(Player player, World world, int level, int[] exit) {
+            Player = player;
+            World = world;
+            Level = level;
+            ExitPos = exit;
+        }
+    }
+}
